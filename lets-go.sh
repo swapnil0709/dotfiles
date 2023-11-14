bfae8745768ee678c32940ef6faf4cf3f12892d2
swapnil0709 <sapnilsinha20@gmail.com>
2023-11-14 19:48:15 +0530

    Handle empty dir case

--- lets-go.sh
+++ lets-go.sh
@@ -27,13 +27,18 @@ Be sure you've read and understood the what will be applied.\033[0m\n"
 
 # If dependencies not met, install them
 if ! hash git 2> /dev/null; then
-  bash <(curl -s  -L 'https://raw.githubusercontent.com/swapnil0709/dotfiles/HEAD/scripts/installs/prerequisites.sh')
+  bash <(curl -sL 'https://raw.githubusercontent.com/swapnil0709/dotfiles/HEAD/scripts/installs/prerequisites.sh')
 fi
 
 # If dotfiles not yet present then clone
 if [[ ! -d "$DOTFILES_DIR" ]]; then
   mkdir -p "${DOTFILES_DIR}" && \
   git clone --recursive ${DOTFILES_REPO} ${DOTFILES_DIR}
+fi
+
+if [-z "$(ls -A ${DOTFILES_DIR})"]; then
+   mkdir -p "${DOTFILES_DIR}" && \
+   git clone --recursive ${DOTFILES_REPO} ${DOTFILES_DIR}
 fi
 
 # Execute setup or update script
